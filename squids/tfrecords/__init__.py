eba4f8af28f736db5ae9d10ac1e71bbbd0caee58
Mykola Galushka <mm.galushka@gmail.com>
2022-02-01 22:09:58 +0000

    Tried to resolve unused import.

--- squids/tfrecords/__init__.py
+++ squids/tfrecords/__init__.py
@@ -2,7 +2,7 @@
 A TFRecords handling module.
 """
 
-from .maker import create_tfrecords  # noqa
-from .loader import load_tfrecords  # noqa
-from .explorer import explore_tfrecords  # noqa
-from .errors import TFRecordsError  # noqa
+from .maker import create_tfrecords  # noqa # pylint: disable=unused-import
+from .loader import load_tfrecords  # noqa # pylint: disable=unused-import
+from .explorer import explore_tfrecords  # noqa # pylint: disable=unused-import
+from .errors import TFRecordsError  # noqa # pylint: disable=unused-import
